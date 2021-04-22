febe30eb352c4e00427b85166861c74caae3b8fa
Sun Myung Park <smpark3@illinois.edu>
2021-04-22 06:29:55 -0500

    Add eigenvalue_scaling input parameter to NtAction for neutron source kernel initializations

--- src/actions/NtAction.C
+++ src/actions/NtAction.C
@@ -69,6 +69,7 @@ validParams<NtAction>()
   params.addRequiredParam<bool>("sss2_input",
                                 "Whether the input follows sss2 form scattering matrices.");
   params.addParam<std::vector<SubdomainName>>("pre_blocks", "The blocks the precursors live on.");
+  params.addParam<Real>("eigenvalue_scaling", 1.0, "Eigenvalue scaling factor.");
   return params;
 }
 
@@ -213,6 +214,7 @@ NtAction::act()
         params.set<unsigned int>("num_groups") = _num_groups;
         params.set<std::vector<VariableName>>("group_fluxes") = all_var_names;
         params.set<bool>("account_delayed") = getParam<bool>("account_delayed");
+        params.set<Real>("eigenvalue_scaling") = getParam<Real>("eigenvalue_scaling");
 
         std::string kernel_name = "CoupledFissionKernel_" + var_name;
         _problem->addKernel("CoupledFissionKernel", kernel_name, params);
@@ -256,6 +258,7 @@ NtAction::act()
           std::vector<std::string> include = {"temperature", "pre_concs"};
           params.applySpecificParameters(parameters(), include);
           params.set<unsigned int>("num_precursor_groups") = _num_precursor_groups;
+          params.set<Real>("eigenvalue_scaling") = getParam<Real>("eigenvalue_scaling");
 
           std::string kernel_name = "DelayedNeutronSource_" + var_name;
           _problem->addKernel("DelayedNeutronSource", kernel_name, params);
