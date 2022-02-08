0c6f4ba0c2afa46f0e5bd4cc69a6be087e292a03
Sun Myung Park <park_cg@live.com>
2022-02-08 17:43:06 -0600

    Edit ElmIntegTotFissNtsPostprocessor to include delayed neutrons

--- include/postprocessors/ElmIntegTotFissNtsPostprocessor.h
+++ include/postprocessors/ElmIntegTotFissNtsPostprocessor.h
@@ -14,8 +14,13 @@ public:
 protected:
   virtual Real computeQpIntegral() override;
 
-  int _num_groups;
+  unsigned int _num_groups;
+  unsigned int _num_precursor_groups;
+  bool _account_delayed;
   const MaterialProperty<std::vector<Real>> & _nsf;
+  const MaterialProperty<Real> & _beta;
+  const MaterialProperty<std::vector<Real>> & _decay_constant;
   std::vector<MooseVariableFEBase *> _vars;
   std::vector<const VariableValue *> _group_fluxes;
+  std::vector<const VariableValue *> _pre_concs;
 };
--- src/postprocessors/ElmIntegTotFissNtsPostprocessor.C
+++ src/postprocessors/ElmIntegTotFissNtsPostprocessor.C
@@ -9,26 +9,44 @@ ElmIntegTotFissNtsPostprocessor::validParams()
   params.addRequiredCoupledVar(
       "group_fluxes",
       "The group fluxes. MUST be arranged by decreasing energy/increasing group number.");
-  params.addRequiredParam<int>("num_groups", "The number of energy groups.");
+  params.addRequiredParam<unsigned int>("num_groups", "The number of energy groups.");
+  params.addCoupledVar("pre_concs", "All the variables that hold the precursor "
+                                    "concentrations. These MUST be listed by increasing "
+                                    "group number.");
+  params.addParam<unsigned int>("num_precursor_groups", 0, "The number of precursor groups.");
+  params.addRequiredParam<bool>("account_delayed", "Whether to account for delayed neutrons.");
   return params;
 }
 
 ElmIntegTotFissNtsPostprocessor::ElmIntegTotFissNtsPostprocessor(const InputParameters & parameters)
   : ElementIntegralPostprocessor(parameters),
     // MooseVariableInterface(this, false),
-    _num_groups(getParam<int>("num_groups")),
+    _num_groups(getParam<unsigned int>("num_groups")),
+    _num_precursor_groups(getParam<unsigned int>("num_precursor_groups")),
+    _account_delayed(getParam<bool>("account_delayed")),
     _nsf(getMaterialProperty<std::vector<Real>>("nsf")),
+    _beta(getMaterialProperty<Real>("beta")),
+    _decay_constant(getMaterialProperty<std::vector<Real>>("decay_constant")),
     _vars(getCoupledMooseVars())
 {
   addMooseVariableDependency(_vars);
-  int n = coupledComponents("group_fluxes");
+  unsigned int n = coupledComponents("group_fluxes");
   if (!(n == _num_groups))
     mooseError("The number of coupled variables doesn't match the number of groups.");
 
   _group_fluxes.resize(n);
   for (unsigned int i = 0; i < _group_fluxes.size(); ++i)
-  {
     _group_fluxes[i] = &coupledValue("group_fluxes", i);
+
+  if (_account_delayed)
+  {
+    unsigned int m = coupledComponents("pre_concs");
+    if (!(m == _num_precursor_groups))
+      mooseError("The number of coupled variables doesn't match the number of precursor groups.");
+
+    _pre_concs.resize(m);
+    for (unsigned int i = 0; i < _pre_concs.size(); ++i)
+      _pre_concs[i] = &coupledValue("pre_concs", i);
   }
 }
 
@@ -36,8 +54,15 @@ Real
 ElmIntegTotFissNtsPostprocessor::computeQpIntegral()
 {
   Real sum = 0;
-  for (int i = 0; i < _num_groups; ++i)
-    sum += _nsf[_qp][i] * (*_group_fluxes[i])[_qp];
+  for (unsigned int i = 0; i < _num_groups; ++i)
+      sum += _nsf[_qp][i] * (*_group_fluxes[i])[_qp];
+
+  if (_account_delayed)
+  {
+    sum *= (1 - _beta[_qp]);
+    for (unsigned int i = 0; i < _num_precursor_groups; ++i)
+      sum += _decay_constant[_qp][i] * (*_pre_concs[i])[_qp];
+  }
 
   return sum;
 }
